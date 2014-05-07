7d6074b3963e64b2e86a4eb3c7334c9e4b53959a
JUAN FRANCISCO MARIN VEGA <jmarinve7.alumnes@ub052028>
2014-05-07 10:41:31 +0200

    creada copia para la parte 2 de la practica 3

---
+++ minikernel_MarinVegaJuan-pract-3-P1/usuario/init.c
@@ -0,0 +1,48 @@
+/*
+ * usuario/init.c
+ *
+ *  Minikernel. Versión 1.0
+ *
+ *  Fernando Pérez Costoya
+ *
+ */
+
+/* Programa inicial que arranca el S.O. Sólo se ejecutarán los programas
+   que se incluyan aquí, que, evidentemente, pueden ejecutar otros
+   programas...
+*/
+
+#include "servicios.h"
+
+int main(){
+
+	printf("init: comienza\n");
+
+    fijar_prio(25);
+    if (crear_proceso("dormilon")<0)
+               printf("Error creando dormilon\n");
+
+    fijar_prio(30);
+    if (crear_proceso("simplon")<0)
+                printf("Error creando simplon\n");
+
+	/* Este programa causa una excepción */
+	if (crear_proceso("excep_arit")<0)
+		printf("Error creando excep_arit\n");
+    fijar_prio(10); 
+    /* Este programa solicita que lo pongan a dormir */
+	if (crear_proceso("dormilon")<0)
+                printf("Error creando dormilon\n");
+
+	/* Este programa crea otro proceso que ejecuta simplon a
+	   una excepción */
+	if (crear_proceso("excep_mem")<0)
+		printf("Error creando excep_mem\n");
+	fijar_prio(50);
+	/* No existe: debe fallar */
+	if (crear_proceso("noexiste")<0)
+		printf("Error creando noexiste\n");
+
+    printf("init: termina\n");
+	return 0; 
+}
